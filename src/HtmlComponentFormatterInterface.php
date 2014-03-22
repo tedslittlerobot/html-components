77d4a89bbd9965b71a6e464e82246df4f73a90d4
Stef Horner <stef@tedslittlerobot.com>
2014-03-22 13:09:41 +0000

    update interface

--- src/HtmlComponentFormatterInterface.php
+++ src/HtmlComponentFormatterInterface.php
@@ -3,40 +3,32 @@
 interface HtmlComponentFormatterInterface {
 
 	/**
-	 * Set the class
-	 * @param  array|string $class
-	 * @return $this
+	 * Open an HTML form
+	 * @param  string  $target
+	 * @param  string  $method
+	 * @param  boolean $files
+	 * @param  array   $attributes
+	 * @param  array   $options
+	 * @return string
 	 */
-	public function class( $class );
-	/**
-	 * Set an attribute
-	 * @param string       $attribute
-	 * @param string|array $value
-	 * @return $this
-	 */
-	public function attribute( $attribute, $value );
-	/**
-	 * Set many attributes
-	 * @param array  $attributes
-	 * @return $this
-	 */
-	public function attributes( array $attributes );
+	public function openForm( $target = '', $method = 'post', $data = false, $attributes = array(), $options = array() );
 
-	public function openForm( $target = '', $method = 'post', $files = false, $attributes = array(), $options() );
 	/**
-	 * Close the form
+	 * Close an HTML form
 	 * @return string
 	 */
 	public function closeForm();
 	public function textInput( $name = null, $value = null, $attributes = array(), $options = array() );
+
+
 	public function textarea( $name = null, $value = null, $attributes = array(), $options = array() );
-	public function input( $type = 'text', $name = null $content = null, $attributes = array(), $options = array() );
+
+
+	public function input( $type = 'text', $name = null, $content = null, $attributes = array(), $options = array() );
 	public function select( $name = null, $items = array(), $attributes = array(), $options = array() );
 	public function select2( $name = null, $items = array(), $attributes = array(), $options = array() );
-	public function checkbox( $name = null, $attributes = array(), $options = array() );
-	public function checkboxes( $name = null, $attributes = array(), $options = array() );
+	public function checkbox( $name = null, $label = null, $attributes = array(), $options = array() );
 	public function radio( $name = null, $attributes = array(), $options = array() );
-	public function radios( $name = null, $attributes = array(), $options = array() );
 	public function button( $content = '', $attributes = array(), $options = array() );
 	public function submit( $content = '', $attributes = array(), $options = array() );
 	public function link( $content, $href, $attributes = array(), $options = array() );
